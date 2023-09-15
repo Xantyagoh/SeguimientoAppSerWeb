d4d59c007441dc416d17cbf9aa9a16937f876a51
SANTIAGO BETANCUR REND�N <santiagobetancur311553@correo.itm.edu.co>
2023-09-14 21:20:26 -0500

    Date.cs class created

---
+++ EmployeeOOP/EmployeeOOP/Clases/Date.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeOOP.Clases
+{
+    public class Date
+    {
+        #region Fields
+
+        private int _year;
+        private int _month;
+        private int _day;
+
+        #endregion
+
+        #region Methods
+
+        public Date(int day, int month, int year)
+        {
+            _year = year;
+            _month = month;
+            _day = day;
+        }
+
+        public override string ToString()
+        {
+           //dia/mes/año
+            var dateConcatenated1 = _day + "/" + _month + "/" + _year; // La mas vieja y fea
+            var dateConcatenated2 = $"{_day}/{_month}/{_year }"; // Interpoblacion
+            var dateConcatenated3 =string.Format("{0}/{1}/{2}", _day, _month, _year); // Class String.Format
+            return dateConcatenated3;
+        }
+
+        #endregion
+    }
+}
--- EmployeeOOP/EmployeeOOP/Program.cs
+++ EmployeeOOP/EmployeeOOP/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using EmployeeOOP.Clases;
+
+Console.WriteLine("OPP APPLICATION");
+Console.WriteLine("----------------");
+
+//Ininicializacion de la clase Date
+Date dateObject = new Date(4, 5, 5655214);
+Console.WriteLine(dateObject.ToString());

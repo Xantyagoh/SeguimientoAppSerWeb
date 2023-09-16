99e2e6b53e5b4bcf0a1ed2a0c53f71ce751e5761
SANTIAGO BETANCUR REND�N <santiagobetancur311553@correo.itm.edu.co>
2023-09-15 20:12:37 -0500

    New abstract class Employee created

--- EmployeeOOP/EmployeeOOP/Clases/Date.cs
+++ EmployeeOOP/EmployeeOOP/Clases/Date.cs
@@ -20,16 +20,83 @@ namespace EmployeeOOP.Clases
 
         public Date(int day, int month, int year)
         {
-            _year = year;
-            _month = month;
-            _day = day;
+            _year = ValidateYear(year);
+            _month = ValidateMonth(month);
+            _day = ValidateDay(day, month, year);
         }
+
+        private int ValidateDay(int day, int month, int year)
+        {
+            //Valido si el dia ingresado pertenece a un año bisiesto:
+            if (month == 2 && day == 29 && IsLeapYear(year))
+            {
+                return day;
+            }
+
+            if (month == 2 && day == 29 && IsLeapYear(year))
+            {
+                bool isLeapYear = false;
+                IsLeapYearException(isLeapYear, year);
+            }
+
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (day >= 1 && day <= daysPerMonth[month])
+            {
+                return day;
+            }
+            else
+            {
+                throw new DayException(String.Format("El dia {0} no es valido para el mes {1}", day, month));
+            }
+        }
+
+        private void IsLeapYearException(bool isLeapYear, int year)
+        {
+            if (!isLeapYear) 
+            {
+                throw new YearException(String.Format("El año {0} no es bisiesto", year));
+            }
+            
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            return (year % 400 == 0 || year % 4 == 0 && year % 100 != 0);
+        }
+
+
+        private int ValidateYear(int year)
+        {
+           if (year >= 1900) 
+            { 
+                return year;
+            }
+            else
+            {
+                throw new YearException(String.Format("El año {0} no es valido", year));
+            }
+        }
+
+
+        private int ValidateMonth(int month)
+        {
+            if(month>=1 && month < 13) //Month = 13
+            {
+                return month;
+            }
+            else
+            {
+                //Exception creation
+                throw new MonthException(String.Format("El mes {0} no es valido", month));
+            }
+        }
+
 
         public override string ToString()
         {
            //dia/mes/año
             var dateConcatenated1 = _day + "/" + _month + "/" + _year; // La mas vieja y fea
-            var dateConcatenated2 = $"{_day}/{_month}/{_year }"; // Interpoblacion
+            var dateConcatenated2 = $"{_day:00}/{_month:00}/{_year:0000}"; // Interpoblacion
             var dateConcatenated3 =string.Format("{0}/{1}/{2}", _day, _month, _year); // Class String.Format
             return dateConcatenated3;
         }
--- EmployeeOOP/EmployeeOOP/Program.cs
+++ EmployeeOOP/EmployeeOOP/Program.cs
@@ -1,9 +1,33 @@
 ﻿
 using EmployeeOOP.Clases;
+try
+{
+    //Declaracion de variables
+    int day, month, year;
 
-Console.WriteLine("OPP APPLICATION");
-Console.WriteLine("----------------");
+    Console.WriteLine("OPP APPLICATION");
+    Console.WriteLine("----------------");
 
-//Ininicializacion de la clase Date
-Date dateObject = new Date(4, 5, 5655214);
-Console.WriteLine(dateObject.ToString());
+    Console.Write("Ingresar el dia: ");
+    day = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresar el mes: ");
+    month = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresar el año: ");
+    year = Convert.ToInt32(Console.ReadLine());
+
+    //Ininicializacion de la clase Date
+    Date dateObject = new Date(day, month, year);
+    Console.WriteLine("\n");
+    Console.WriteLine(dateObject.ToString());
+
+}
+
+catch (Exception ex)
+{
+
+	Console.WriteLine(ex.Message);
+}
+
+
